d730e10712b803c0f33b10bd30ee3f7f282ece50
VANGAZOR <asei3k@gmail.com>
2023-07-13 10:55:27 +0300

    auth changes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const express = require("express")
+const router = express.Router();
 const auth = require("../middlewares/auth");
 const userRoutes = require("./users");
 const moviesRoutes = require("./movies");
@@ -9,7 +10,5 @@ router.use("/", authRouters);
 router.use(auth);
 router.use("/users", userRoutes);
 router.use("/movies", moviesRoutes);
-
-router.use(express.json());
 
 module.exports = router;
