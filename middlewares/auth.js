68338a823ff0b3360c44da73f72d902cab3884e8
VANGAZOR <asei3k@gmail.com>
2023-07-13 09:14:06 +0300

    auth changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,23 @@
 const jwt = require("jsonwebtoken");
-const { HTTP_STATUS_UNAUTHORIZED} =
-  require("http2").constants;
+const AuthError = require("../errors/AuthError");
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res.status(HTTP_STATUS_UNAUTHORIZED).send({ message: "Необходима авторизация" });
+    return next(new AuthError("Необходима авторизация"));
   }
   const token = authorization.replace("Bearer ", "");
   let payload;
   try {
-    payload = jwt.verify(token, "some-secret-key");
+    payload = jwt.verify(
+      token,
+      NODE_ENV === "production" ? JWT_SECRET : "dev-secret"
+    );
   } catch (err) {
-    return res.status(HTTP_STATUS_UNAUTHORIZED).send({
-      message: `Необходима авторизация`,
-    });
+    return next(new AuthError("Необходима авторизация"));
   }
   req.user = payload;
-  next();
+  return next();
 };
